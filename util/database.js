fed703e8a09d5a6235a9b556ea4aa1b651b0adbc
Fisayo AGBOOLA <fisayo.agboola@ariosh.com>
2020-04-03 19:21:42 +0100

    Added Docker and docker-compose files

--- util/database.js
+++ util/database.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
+require('dot-env')
 
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+const user = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const postgresDb = process.env.POSTGRES_DB;
+
+const sequelize = new Sequelize(postgresDb, user, password, {
     dialect: 'postgres', 
     host: 'localhost'
 });
